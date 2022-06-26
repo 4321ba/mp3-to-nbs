19c2e62f0d81dc69c6948c4325dd57b076453f1c
1234ab <nevenincs007@gmail.com>
2022-06-26 19:09:13 +0200

    add babycat dependency and minimal main()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use babycat::{Signal, Waveform, WaveformArgs};
+
+fn maxslice(slice: &[f32]) -> &f32 {
+    slice
+        .iter()
+        .max_by(|a,  b| a.partial_cmp(b).expect("No nan!!")).expect("slice shouldnt be empty")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let waveform_args = WaveformArgs {
+        convert_to_mono: true,
+        ..Default::default()
+    };
+    let waveform =
+        match Waveform::from_file("musictests/olddiscjing.mp3", waveform_args) {
+            Ok(w) => w,
+            Err(err) => {
+                println!("Decoding error: {}", err);
+                return;
+            }
+        };
+    println!(
+        "Decoded {} frames with {} channels at {} hz",
+        waveform.num_frames(),
+        waveform.num_channels(),
+        waveform.frame_rate_hz(),
+    );
+    //let samples: &[f32] = waveform.to_interleaved_samples();
+    println!("{:?}", &waveform.to_interleaved_samples()[105750..105800]);
+    println!("{}", maxslice(waveform.to_interleaved_samples()));
 }
